565f68ff09cbb42b95c9e270ede136207cad1d62
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-02-08 00:34:00 +0100

    Implement app header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled from '@emotion/styled';
 import { H3 } from './components/headings';
+import { MediumButton } from './components/buttons';
+
+const AppHeader = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <H3>Top 100</H3>
+        <AppHeader>
+          <H3>Top 100</H3>
+          <MediumButton icon="plus">Add top 10</MediumButton>
+        </AppHeader>
       </div>
     );
   }
