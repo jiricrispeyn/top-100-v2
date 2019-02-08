5738f01c4b033fbeebff91270ab686df00460257
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-02-08 19:53:18 +0100

    Add basic router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
+import { Router } from '@reach/router';
+import Home from './views/home';
+import AddList from './views/add-list';
 import './App.css';
-import styled from '@emotion/styled';
-import { H3 } from './components/headings';
-import { MediumButton } from './components/buttons';
-
-const AppHeader = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AppHeader>
-          <H3>Top 100</H3>
-          <MediumButton icon="plus">Add top 10</MediumButton>
-        </AppHeader>
+        <Router>
+          <Home path="/" />
+          <AddList path="add" />
+        </Router>
       </div>
     );
   }
