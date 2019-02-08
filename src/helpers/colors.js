c1f7de793a1fd317691f7433e957e685142fad9b
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-02-08 19:54:05 +0100

    Create color helpers

---
+++ src/helpers/colors.js
@@ -0,0 +1,18 @@
+import { rgba } from 'polished';
+
+export default {
+  blue: '#2E5BFF',
+  lightBlue: rgba('#2E5BFF', 0.2),
+  green: '#33AC2E',
+  red: '#D63649',
+  yellow: '#F7C137',
+  teal: '#00C1D4',
+  purple: '#8C54FF',
+  black: '#2E384D',
+  black2: '#69707F',
+  black3: '#8798AD',
+  white: '#FFFFFF',
+  gray: '#BFC5D2',
+  gray2: '#F4F6FC',
+  gray3: '#EEF3F5',
+};
