297a96ce79693830cc32530a065564fa8d8d565e
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-02-10 00:53:24 +0100

    Create styled form elements

---
+++ src/components/form/style.js
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+import { rgba } from 'polished';
+import colors from '../../helpers/colors';
+
+export const Input = styled.input`
+  padding: 0.625rem 1rem;
+  background-color: ${rgba('#E0E7FF', 0.2)};
+  border: 1px solid #e0e7ff;
+  border-radius: 0.3125rem;
+  font-size: 0.9375rem;
+  line-height: 1.375;
+
+  &:focus,
+  &:active {
+    outline: none;
+  }
+
+  &::placeholder {
+    color: ${colors.black3};
+  }
+`;
