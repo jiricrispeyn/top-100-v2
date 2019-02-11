10cfb867e6921f0d87a456504d9bce1c3264cff9
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-02-11 18:49:06 +0100

    Add Spotify token middleware

--- src/api/queries.js
+++ src/api/queries.js
@@ -1,12 +1,50 @@
 import { API_URL } from './constants';
-import { headers } from './';
 
+/**
+ * Get Search Spotify Catalog information about artists, albums, tracks or playlists that match a keyword string.
+ *
+ * @param {String} query Search query keywords and optional field filters and operators.
+ * @param {String} type A comma-separated list of item types to search across. Valid types are: album , artist, playlist, and track.
+ * @returns
+ */
 export const search = async (query, type) => {
+  const headers = await getHeaders();
   const response = await fetch(
     `${API_URL}/v1/search?query=${query}&type=${type}`,
-    { headers }
+    {
+      headers,
+    }
   );
-  const json = await response.json();
+  const data = await response.json();
 
-  return json;
+  return data;
 };
+
+/**
+ * Fetches valid access token for the Spotify Web API
+ *
+ * @returns Access token
+ */
+async function getToken() {
+  try {
+    const response = await fetch('http://localhost:8888/token');
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+/**
+ * Constructs HTTP Headers for fetching the Sptofiy Web API
+ *
+ * @returns Headers
+ */
+async function getHeaders() {
+  const { token } = await getToken();
+
+  return {
+    Authorization: `Bearer ${token}`,
+  };
+}
