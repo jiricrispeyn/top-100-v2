33443253298dd751136834f1661bbb471a980f70
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-02-12 17:08:49 +0100

    Fix typo

--- src/components/card/style.js
+++ src/components/card/style.js
@@ -26,7 +26,7 @@ export const StyledIntegrationCard = styled.div`
   border-radius: 5px;
   border: 1px solid #e0e7ff;
 
-  &:not(::nth-of-type(1)) {
+  &:not(:nth-of-type(1)) {
     margin-left: 1.5rem;
   }
 `;
