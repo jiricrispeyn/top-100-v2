e516c6fed14d128f13d111a7f37d36b99151fdbf
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-09-18 15:39:44 +0200

    Remove apollo graphql

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import { Router } from '@reach/router';
 import Home from './views/home';
 import AddList from './views/add-list';
 import './App.css';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-
-const client = new ApolloClient();
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ApolloProvider client={client}>
-          <Router>
-            <Home path="/" />
-            <AddList path="add" />
-          </Router>
-        </ApolloProvider>
+        <Router>
+          <Home path="/" />
+          <AddList path="add" />
+        </Router>
       </div>
     );
   }
