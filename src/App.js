800bcfaf83c3a6fba540662f1fe1f47245eb2fc6
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-09-18 11:37:58 +0200

    Setup apollo graphql

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import { Router } from '@reach/router';
 import Home from './views/home';
 import AddList from './views/add-list';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient();
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          <Home path="/" />
-          <AddList path="add" />
-        </Router>
+        <ApolloProvider client={client}>
+          <Router>
+            <Home path="/" />
+            <AddList path="add" />
+          </Router>
+        </ApolloProvider>
       </div>
     );
   }
