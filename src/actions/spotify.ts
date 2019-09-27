d333c8e82531d3bc7ce2a02ffed076922c84d0bf
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-09-27 21:33:18 +0100

    Add spotify search api actions

--- src/actions/spotify.ts
+++ src/actions/spotify.ts
@@ -1,15 +1,33 @@
 import { AnyAction } from 'redux';
-import { createUiActionType, createUiAction } from './utils';
+import {
+  createUiActionType,
+  createUiAction,
+  createApiActionTypes,
+  createApiActions,
+  ApiActionTypes,
+} from './utils';
 
-interface PermissionActionTypes {
+interface SpotifyActionTypes {
   SEARCH: string;
 }
 
-export const uiActionTypes: PermissionActionTypes = {
+interface SpotifyApiActionTypes {
+  SEARCH: ApiActionTypes;
+}
+
+export const uiActionTypes: SpotifyActionTypes = {
   SEARCH: createUiActionType('SPOTIFY/SEARCH'),
 };
 
 export const uiActions = {
   search: (query: string, type: string): AnyAction =>
     createUiAction(uiActionTypes.SEARCH, { query, type }),
+};
+
+export const apiActionTypes: SpotifyApiActionTypes = {
+  SEARCH: createApiActionTypes('SPOTIFY/SEARCH'),
+};
+
+export const apiActions = {
+  search: createApiActions(apiActionTypes.SEARCH),
 };
