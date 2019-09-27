048379281c9d17c19e30968128f1c8337144b1eb
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-09-27 21:34:10 +0100

    Execute search api call in search saga

--- src/sagas/spotify.ts
+++ src/sagas/spotify.ts
@@ -1,14 +1,23 @@
 import { AnyAction } from 'redux';
 import { SagaIterator } from 'redux-saga';
-import { takeLatest } from 'redux-saga/effects';
+import { call, takeLatest } from 'redux-saga/effects';
 
 import actions from '../actions';
-// import { apiCall } from './utils';
+import { apiCall } from './utils';
+import { search as searchFn } from '../api/queries';
+
+export const searchApiCall = apiCall.bind(
+  null,
+  actions.spotify.apiActions.search,
+  searchFn
+);
 
 export const search = function*(action: AnyAction): SagaIterator {
   const { query, type } = action.payload;
 
   console.log({ query, type });
+
+  yield call(searchApiCall, { query, type });
 };
 
 export const watchSearch = function*(): SagaIterator {
