9b909ae5b08a73f972f3e25c988b0abdc186b38b
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-10-02 22:46:15 +0200

    Add top-100 actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,4 @@
 import * as spotify from './spotify';
+import * as top100 from './top-100';
 
-export default { spotify };
+export default { spotify, top100 };
--- src/actions/top-100.ts
+++ src/actions/top-100.ts
@@ -7,26 +7,32 @@ import {
   ApiActionTypes,
 } from './utils';
 
-interface SpotifyActionTypes {
+interface Top100ActionTypes {
   ADD: string;
+  GET: string;
 }
 
-interface SpotifyApiActionTypes {
+interface Top100ApiActionTypes {
   ADD: ApiActionTypes;
+  GET: ApiActionTypes;
 }
 
-export const uiActionTypes: SpotifyActionTypes = {
+export const uiActionTypes: Top100ActionTypes = {
   ADD: createUiActionType('TOP_100/ADD'),
+  GET: createUiActionType('TOP_100/GET'),
 };
 
 export const uiActions = {
   add: (list: any[]): AnyAction => createUiAction(uiActionTypes.ADD, { list }),
+  get: (): AnyAction => createUiAction(uiActionTypes.GET, {}),
 };
 
-export const apiActionTypes: SpotifyApiActionTypes = {
+export const apiActionTypes: Top100ApiActionTypes = {
   ADD: createApiActionTypes('TOP_100/ADD'),
+  GET: createApiActionTypes('TOP_100/GET'),
 };
 
 export const apiActions = {
   add: createApiActions(apiActionTypes.ADD),
+  get: createApiActions(apiActionTypes.GET),
 };
